ff1f18dda91c6fa81aa0518365110f32cbf0d457
Jose Baeza <josedelarosabass@gmail.com>
2025-06-24 22:34:07 -0600

    feat: create experiencie collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,27 @@
 import { z, defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
-const tech  = defineCollection({
+const tech = defineCollection({
   loader: glob({ pattern: "**/*.yml", base: "./src/data/tech" }),
-  schema: ({image}) => z.object({
-    order: z.number().int().positive(),
-    icon: image(),
-    tech: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      order: z.number().int().positive(),
+      icon: image(),
+      tech: z.string(),
+    }),
 });
 
+const companies = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/companies" }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+      company: z.string(),
+      start: z.string(),
+      end: z.string(),
+    }),
+});
 
 // Expose your defined collection to Astro
 // with the `collections` export
-export const collections = { tech };
+export const collections = { tech, companies };
---
+++ src/data/companies/heb.md
@@ -0,0 +1,8 @@
+---
+title:  Analista Empresarial
+company: Heb
+start: 16-12-2024
+end: Actualidad
+---
+
+Responsable de analizar y documentar los procesos empresariales, identificando oportunidades de mejora proponiendo soluciones tecnológicas para optimizar la eficiencia operativa, solventando las necesidades de nuestros stakeholders. Usando herramientas de modelado como  Microsoft Visio, herramientas de gestión de proyectos como Azure, étc.
